8af0f8ccb260446ed5129118dd43262996b6efdd
Gadgeon <gadgeon@mouzmis-mac-mini.local>
2015-12-01 14:58:22 +0530

    Created custom classes for SnapCam View

---
+++ iONLive/iONLiveUI/Settings/SnapCamCustomHeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  SnapCamCustomHeaderCell.swift
+//  iONLive
+//
+//  Created by Gadgeon on 12/1/15.
+//  Copyright © 2015 Gadgeon. All rights reserved.
+//
+
+import UIKit
+
+class SnapCamCustomHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
