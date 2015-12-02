4cdaa0c07cc6333d5d5e6ba8ffddcf8bb3525782
Gadgeon <gadgeon@mouzmis-mac-mini.local>
2015-12-02 12:40:16 +0530

    Added Streaming gallery controller

---
+++ iONLive/iONLiveUI/Streaming/StreamsGalleryViewController.swift
@@ -0,0 +1,23 @@
+//
+//  StreamsGalleryViewController.swift
+//  iONLive
+//
+//  Created by Gadgeon on 12/2/15.
+//  Copyright © 2015 Gadgeon. All rights reserved.
+//
+
+import UIKit
+
+class StreamsGalleryViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
