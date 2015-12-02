c831595f4fbfebd0f00d8898928c506ffda2ab40
Gadgeon <gadgeon@mouzmis-mac-mini.local>
2015-12-02 15:17:11 +0530

    StreamListView UI

---
+++ iONLive/iONLiveUI/Streaming/StreamListCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  StreamListCollectionViewCell.swift
+//  iONLive
+//
+//  Created by Gadgeon on 12/2/15.
+//  Copyright © 2015 Gadgeon. All rights reserved.
+//
+
+import UIKit
+
+class StreamListCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var liveStatusLabel: UILabel!
+    @IBOutlet weak var liveNowIcon: UIImageView!
+    @IBOutlet weak var streamThumbnaleImageView: UIImageView!
+}
