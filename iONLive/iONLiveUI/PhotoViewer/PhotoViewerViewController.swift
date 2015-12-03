32795c95edb79cd5c13259a354946f3db030dd12
Gadgeon <gadgeon@mouzmis-mac-mini.local>
2015-12-03 16:53:59 +0530

    Added assets, Created controller and storyboard for PhotoViewe

---
+++ iONLive/iONLiveUI/PhotoViewer/PhotoViewerViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoViewerViewController.swift
+//  iONLive
+//
+//  Created by Gadgeon on 12/3/15.
+//  Copyright © 2015 Gadgeon. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewerViewController: UIViewController {
+
+    static let identifier = "PhotoViewerViewController"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
