3be66c319746e7d873827a46ae44c0c7adaedf8f
shamlykh <shamly.kh@gadgeon.com>
2016-04-25 16:03:29 +0530

    app delegate and stream pull

--- iONLive/AppDelegate.swift
+++ iONLive/AppDelegate.swift
@@ -13,10 +13,11 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var notificationFlag : Bool = false
 
     var photoViewController : PhotoViewerViewController?
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    
+       
         let settings : UIUserNotificationSettings = UIUserNotificationSettings(forTypes:[UIUserNotificationType.Alert, UIUserNotificationType.Sound], categories: nil)
         UIApplication.sharedApplication().registerUserNotificationSettings(settings)
         UIApplication.sharedApplication().registerForRemoteNotifications()
@@ -31,11 +32,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(application: UIApplication) {
+        
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
+            
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
@@ -45,6 +48,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
+        if notificationFlag == true
+        {
+            notificationFlag = false
+            let defaults = NSUserDefaults .standardUserDefaults()
+            defaults.setValue("1", forKey: "notificationFlag")
+            var navigationController:UINavigationController?
+            let notificationStoryboard = UIStoryboard(name:"MyChannel" , bundle: nil)
+            let notificationViewController = notificationStoryboard.instantiateViewControllerWithIdentifier(MyChannelNotificationViewController.identifier) as! MyChannelNotificationViewController
+            navigationController = UINavigationController(rootViewController: notificationViewController)
+            navigationController!.navigationBarHidden = true
+            
+            self.window!.rootViewController = navigationController
+        }
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
@@ -184,16 +200,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
         print(userInfo)
-//        
-//        let defaults = NSUserDefaults .standardUserDefaults()
-//        defaults.setValue("1", forKey: "notificationFlag")
-        var navigationController:UINavigationController?
-        let notificationStoryboard = UIStoryboard(name:"MyChannel" , bundle: nil)
-        let notificationViewController = notificationStoryboard.instantiateViewControllerWithIdentifier(MyChannelNotificationViewController.identifier) as! MyChannelNotificationViewController
-        navigationController = UINavigationController(rootViewController: notificationViewController)
-        navigationController!.navigationBarHidden = true
-        
-        self.window!.rootViewController = navigationController
+            notificationFlag = true
     }
     
     //Called if unable to register for APNS.
