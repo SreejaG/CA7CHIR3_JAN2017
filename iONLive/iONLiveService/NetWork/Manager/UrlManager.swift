fc020eff311d9f6c6a36b9bbae3197358e1556b9
Sreeja <sreeja@sreeja-macmini2.local>
2016-09-22 15:46:36 +0530

    Swiping during video play in my media screen

--- iONLive/iONLiveService/NetWork/Manager/UrlManager.swift
+++ iONLive/iONLiveService/NetWork/Manager/UrlManager.swift
@@ -4,9 +4,9 @@ class UrlManager {
     
    // let baseUrl = "http://192.168.18.89:3000";
     
-//    let baseUrl = "http://104.196.113.247:3000"; //Production
+    let baseUrl = "http://104.196.113.247:3000"; //Production
     
-    let baseUrl = "http://104.197.92.137:3000"; //test instance
+//    let baseUrl = "http://104.197.92.137:3000"; //test instance
     
     let iONLiveCamUrl = "http://104.197.92.137:8888"
     
